c07da92b4174b082c10bd0cfe6424fe44f2fa710
Shalini2702 <113661286+Shalini2702@users.noreply.github.com>
2022-11-25 05:54:24 -0800

    Create child.py

---
+++ Final deliverables/child.py
@@ -0,0 +1,24 @@
+  import time
+	import wiotp.sdk.application
+	myConfig={
+		"identity":{
+			"orgId":"af8k8g",
+			"typeId":"Tracker",
+			"deviceId":"12345",
+		},
+		"auth":{
+			"token":"12345678"
+		}
+	}
+	
+	client=wiotp.sdk.device.DeviceClient(config=myConfig,logHandlers=None)
+	client.connect()
+	while True:
+		name="Child"
+		latitude=17.4219272
+		longitude=78.5488783
+		myData={'name':name,'lat':latitude,'lon':longitude}
+		client.publishEvent(eventId="status",msgFormat="json",data=myData,qos=0,onPublish=None)
+		print("Data published to IBM IoT Platform: ",myData)
+		time.sleep(5)
+	client.disconnect()
